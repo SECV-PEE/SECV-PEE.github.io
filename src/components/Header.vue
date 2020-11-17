2f69222ee6a7a93653bb2e2816869fac4f990493
kberisha42 <kberisha42@student.42.fr>
2020-11-17 13:31:36 +0100

    remove bsvue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,28 +8,6 @@
       </div>
       <img id="logo-pref" :src="`${path}logo_prif.png`" width="185" height="150" alt="logo">
     </div>
-    <b-navbar class="header" toggleable="lg" type="dark" fixed="top" variant="info" v-b-scrollspy:nav-scroller>
-      <b-navbar-toggle target="navbar-collapse"></b-navbar-toggle>
-      <b-collapse id="navbar_collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item :to="{name: 'tdb'}">Chiffres clés</b-nav-item>
-          <b-nav-item-dropdown text="Bilans thématiques">
-            <b-dropdown-item :to="{name: 'conso'}">Consommations énergétiques</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'emiss'}">Émissions de GES</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'air'}">Qualité de l'air</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item-dropdown text="Bilans sectoriels">
-            <b-dropdown-item :to="{name: 'reno'}">Rénovation énergétique</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'preca'}">Précarité énergétique</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'mobi'}">Transports</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'chal'}">Chaleur renouvelable</b-dropdown-item>
-            <b-dropdown-item :to="{name: 'enr'}">Autres ENR</b-dropdown-item>
-          </b-nav-item-dropdown>
-          <b-nav-item to="/tableau-de-bord">Liens utiles</b-nav-item>
-          <b-nav-item to="/tableau-de-bord">Nous contacter</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,15 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import VS2 from 'vue-script2'
 import router from './router'
-import BootstrapVue from 'bootstrap-vue'
 
 Vue.use(VS2)
 Vue.use(require('vue-script2'))
-Vue.use(BootstrapVue)
 Vue.config.productionTip = false
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   el: '#app',
